62ba7ed359ffc2caa1d035f653ef6bf0152990db
Duy Nguyen <dooz127@gmail.com>
2021-05-13 20:13:55 -0700

    Fix stripe webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,7 +21,9 @@ exports.createCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`${req.protocol}://${req.get('host')}/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
